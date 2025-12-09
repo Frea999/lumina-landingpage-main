ca58bf130f7a029ca8399ed69c45276b933c23dc
Frea <fredrikmoellenbergdot@gmail.com>
2025-12-09 17:48:10 +0100

    added-js-and-fixed-a-bit-of-css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const speaker = [
+    { src: 'img/speaker-rose.png' },
+    { src: 'img/green1.png' },
+    { src: 'img/white.png' },
+    { src: 'img/lavender.png' },
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+    const changeColorBtn = document.getElementById('color-change-btn');
+    const speakerImage = document.querySelector('.speaker-rose img');
+    
+    let currentColorIndex = 0;
+
+    changeColorBtn.addEventListener('click', function() {
+        currentColorIndex = (currentColorIndex + 1)
+        speakerImage.src = speaker[currentColorIndex].src;
+        
+    });
+});
+
